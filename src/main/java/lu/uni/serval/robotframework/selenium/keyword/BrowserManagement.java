22edc18f87992e920696aa612efee125dd5529d1
kabinja <renaudrwemalika@gmail.com>
2018-02-05 18:09:46 +0100

    add selenium API calls MaximizeBrowserWindow and SetSeleniumSpeed

--- src/main/java/lu/uni/serval/robotframework/selenium/keyword/BrowserManagement.java
+++ src/main/java/lu/uni/serval/robotframework/selenium/keyword/BrowserManagement.java
@@ -4,9 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class BrowserManagement {
     private WebDriver driver;
+
+    @Keyword
+    public void MaximizeBrowserWindow(List<String> arguments) {
+        driver.manage().window().maximize();
+    }
 
     @Keyword
     public void OpenBrowser(List<String> arguments){
@@ -15,6 +21,13 @@ public class BrowserManagement {
         }
 
         driver.get(arguments.get(0));
+    }
+
+    @Keyword
+    public void SetSeleniumSpeed(List<String> arguments) {
+        //TODO: properly convert time
+        int time = Integer.parseInt(arguments.get(0));
+        driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
     }
 
     void NonKeyword(){
