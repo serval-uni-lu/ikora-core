3e834dbea4b52fd531e5f3b9b02153cced2ae417
kabinja <renaudrwemalika@gmail.com>
2018-06-16 17:07:31 +0200

    add create method to Line class

--- src/main/java/lu/uni/serval/robotframework/parser/Line.java
+++ src/main/java/lu/uni/serval/robotframework/parser/Line.java
@@ -1,19 +1,37 @@
 package lu.uni.serval.robotframework.parser;
 
+import java.io.IOException;
+import java.io.LineNumberReader;
+
 public class Line {
     private String text;
-    private int lineNumber;
+    private int number;
 
-    public Line(int lineNumber, String text) {
-        this.text = text;
-        this.lineNumber = lineNumber;
+    private Line() {
+        this.number = 0;
     }
 
-    public int getLineNumber() {
-        return lineNumber;
+    public int getNumber() {
+        return number;
     }
 
     public String getText() {
         return text;
+    }
+
+    public boolean isValid() {
+        return text != null;
+    }
+
+    public static Line getNextLine(LineNumberReader reader) throws IOException {
+        Line line = new Line();
+
+        line.text = reader.readLine();
+
+        if(line.text != null) {
+            line.number = reader.getLineNumber();
+        }
+
+        return line;
     }
 }
