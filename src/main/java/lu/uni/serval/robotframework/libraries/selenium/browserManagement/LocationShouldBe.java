decc5da736c9104effc298eccf6a0e4dcf523e07
kabinja <renaudrwemalika@gmail.com>
2018-06-18 10:49:34 +0200

    add new version of selenium library

---
+++ src/main/java/lu/uni/serval/robotframework/libraries/selenium/browserManagement/LocationShouldBe.java
@@ -0,0 +1,4 @@
+package lu.uni.serval.robotframework.libraries.selenium.browserManagement;
+
+public class LocationShouldBe {
+}
---
+++ src/main/java/lu/uni/serval/robotframework/model/LibraryKeyword.java
@@ -0,0 +1,22 @@
+package lu.uni.serval.robotframework.model;
+
+import lu.uni.serval.utils.tree.TreeNodeData;
+
+public class LibraryKeyword implements TreeNodeData {
+
+
+    @Override
+    public String getLabel() {
+        return null;
+    }
+
+    @Override
+    public boolean isSame(TreeNodeData other) {
+        return false;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+}
