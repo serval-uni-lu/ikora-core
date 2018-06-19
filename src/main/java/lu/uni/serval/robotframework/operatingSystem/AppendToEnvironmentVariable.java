4e358360c301ca91991079a1598a323dec0a2f9d
kabinja <renaudrwemalika@gmail.com>
2018-06-19 18:28:03 +0200

    add operatingSystem until DirectoryShouldExist from http://robotframework.org/robotframework/latest/libraries/OperatingSystem.html keyword list

---
+++ src/main/java/lu/uni/serval/robotframework/operatingSystem/AppendToEnvironmentVariable.java
@@ -0,0 +1,10 @@
+package lu.uni.serval.robotframework.operatingSystem;
+
+import lu.uni.serval.robotframework.model.LibraryKeyword;
+
+public class AppendToEnvironmentVariable extends LibraryKeyword {
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/main/java/lu/uni/serval/robotframework/operatingSystem/AppendToFile.java
@@ -0,0 +1,10 @@
+package lu.uni.serval.robotframework.operatingSystem;
+
+import lu.uni.serval.robotframework.model.LibraryKeyword;
+
+public class AppendToFile extends LibraryKeyword {
+    @Override
+    public void execute() {
+
+    }
+}
