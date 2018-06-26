975d7079865c85d739ac89fce0f20f1be79d82ce
kabinja <renaudrwemalika@gmail.com>
2018-06-26 09:21:44 +0200

    create KeywordStatus

---
+++ src/main/java/lu/uni/serval/robotframework/report/KeywordStatus.java
@@ -0,0 +1,42 @@
+package lu.uni.serval.robotframework.report;
+
+import lu.uni.serval.robotframework.model.Keyword;
+
+import java.time.LocalDateTime;
+
+public class KeywordStatus {
+    public enum Status{
+        PASS, FAIL, IGNORED
+    }
+
+    private Status status;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Keyword keyword;
+    private String log;
+
+    public KeywordStatus() {
+        this.status = Status.IGNORED;
+        this.keyword = null;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime
+    }
+
+    public void setKeyword(Keyword keyword) {
+        this.keyword = keyword;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+}
