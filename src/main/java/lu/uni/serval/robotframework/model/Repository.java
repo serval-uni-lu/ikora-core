7d689bd15c613c04641e342cb4c26fcddf450448
kabinja <renaudrwemalika@gmail.com>
2018-06-27 09:23:42 +0200

    link git api to project

--- src/main/java/lu/uni/serval/robotframework/model/Repository.java
+++ src/main/java/lu/uni/serval/robotframework/model/Repository.java
@@ -1,13 +1,21 @@
 package lu.uni.serval.robotframework.model;
 
-import java.net.URL;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
 import java.time.LocalDateTime;
 
 public class Repository {
-    private URL url;
+    private Git git;
 
-    public Repository(String url){
-
+    public Repository(String url, String localPath)  {
+        try {
+            clone(url, localPath);
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+            git = null;
+        }
     }
 
     public TestCase findTestCase(String source, String name) {
@@ -16,5 +24,12 @@ public class Repository {
 
     public void checkout(LocalDateTime dateTime) {
 
+    }
+
+    private void clone(String url, String localPath) throws GitAPIException {
+        this.git = Git.cloneRepository()
+                .setURI(url)
+                .setDirectory(new File(localPath))
+                .call();
     }
 }
