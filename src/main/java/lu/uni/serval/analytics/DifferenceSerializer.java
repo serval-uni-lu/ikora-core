38841782481c04b325e24cbeeabb20ec9a122b06
kabinja <renaudrwemalika@gmail.com>
2018-07-10 20:49:40 +0200

    improve the way differences are expressed when adding and removing user keyword

--- src/main/java/lu/uni/serval/analytics/DifferenceSerializer.java
+++ src/main/java/lu/uni/serval/analytics/DifferenceSerializer.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import lu.uni.serval.robotframework.model.Argument;
 import lu.uni.serval.robotframework.model.KeywordCall;
 import lu.uni.serval.robotframework.model.KeywordDefinition;
+import lu.uni.serval.robotframework.model.Step;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,18 +18,13 @@ public class DifferenceSerializer extends JsonSerializer<Difference> {
     public void serialize(Difference difference, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
         jsonGenerator.writeStartObject();
 
-        if(difference.getLeft() != null && difference.getRight() != null){
-            writeKeywordInfo(jsonGenerator, difference.getLeft(), "before");
-            writeKeywordInfo(jsonGenerator, difference.getRight(), "after");
-        }
-
         writeActions(difference, jsonGenerator);
 
         jsonGenerator.writeEndObject();
     }
 
     private void writeActions(Difference difference, JsonGenerator jsonGenerator) throws IOException {
-        jsonGenerator.writeArrayFieldStart("differences");
+        jsonGenerator.writeArrayFieldStart("actions");
 
         KeywordDefinition before = difference.getLeft();
         KeywordDefinition after = difference.getRight();
@@ -39,22 +35,22 @@ public class DifferenceSerializer extends JsonSerializer<Difference> {
             switch (action.getType()){
                 case ADD_USER_KEYWORD:
                 {
-                    jsonGenerator.writeStringField("action", "add user keyword");
-                    jsonGenerator.writeStringField("user keyword", after.getName().toString());
+                    jsonGenerator.writeStringField("type", "add user keyword");
+                    writeKeywordInfo(jsonGenerator, after, "user keyword", true);
                 }
                 break;
 
                 case REMOVE_USER_KEYWORD:
                 {
-                    jsonGenerator.writeStringField("action", "remove user keyword");
-                    jsonGenerator.writeStringField("user keyword", before.getName().toString());
+                    jsonGenerator.writeStringField("type", "remove user keyword");
+                    writeKeywordInfo(jsonGenerator, before, "user keyword", true);
                 }
                 break;
 
                 case ADD_STEP:
                 {
                     int position = action.getRight();
-                    jsonGenerator.writeStringField("action", "add step");
+                    jsonGenerator.writeStringField("type", "add step");
                     jsonGenerator.writeStringField("step", after.getStep(position).getName().toString());
                     jsonGenerator.writeNumberField("position", position);
                 }
@@ -63,7 +59,7 @@ public class DifferenceSerializer extends JsonSerializer<Difference> {
                 case REMOVE_STEP:
                 {
                     int position = action.getLeft();
-                    jsonGenerator.writeStringField("action", "remove step");
+                    jsonGenerator.writeStringField("type", "remove step");
                     jsonGenerator.writeStringField("step", after.getStep(position).getName().toString());
                     jsonGenerator.writeNumberField("position", position);
                 }
@@ -71,7 +67,7 @@ public class DifferenceSerializer extends JsonSerializer<Difference> {
 
                 case CHANGE_STEP_NAME:
                 {
-                    jsonGenerator.writeStringField("action", "change name");
+                    jsonGenerator.writeStringField("type", "change name");
 
                     int positionBefore = action.getLeft();
                     jsonGenerator.writeStringField("before", before.getStep(positionBefore).getName().toString());
@@ -83,7 +79,7 @@ public class DifferenceSerializer extends JsonSerializer<Difference> {
 
                 case CHANGE_STEP_ARGUMENTS:
                 {
-                    jsonGenerator.writeStringField("action", "change arguments");
+                    jsonGenerator.writeStringField("type", "change arguments");
 
                     int positionBefore = action.getLeft();
                     KeywordCall callBefore = (KeywordCall) before.getStep(positionBefore);
@@ -96,17 +92,32 @@ public class DifferenceSerializer extends JsonSerializer<Difference> {
                 break;
             }
 
+            if(difference.getLeft() != null && difference.getRight() != null){
+                writeKeywordInfo(jsonGenerator, before, "keyword name before", false);
+                writeKeywordInfo(jsonGenerator, after, "keyword name after", false);
+            }
+
             jsonGenerator.writeEndObject();
         }
 
         jsonGenerator.writeEndArray();
     }
 
-    private void writeKeywordInfo(JsonGenerator jsonGenerator, KeywordDefinition keyword, String name) throws IOException {
+    private void writeKeywordInfo(JsonGenerator jsonGenerator, KeywordDefinition keyword, String name, boolean showSteps) throws IOException {
         jsonGenerator.writeObjectFieldStart(name);
 
         jsonGenerator.writeStringField("file", keyword.getFile());
         jsonGenerator.writeStringField("name", keyword.getName().toString());
+
+        if(showSteps){
+            jsonGenerator.writeArrayFieldStart("steps");
+
+            for(Step step: keyword){
+                jsonGenerator.writeString(step.getName().toString());
+            }
+
+            jsonGenerator.writeEndArray();
+        }
 
         jsonGenerator.writeEndObject();
     }
