0ffa16ba38b8394e889b6244d206cc919c44ea9b
kabinja <renaudrwemalika@gmail.com>
2018-07-25 13:09:14 +0200

    add tests for tokenizer in compiler.Line

---
+++ src/test/java/lu/uni/serval/robotframework/compiler/LineTest.java
@@ -0,0 +1,42 @@
+package lu.uni.serval.robotframework.compiler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class LineTest {
+
+    @Test
+    public void checkTokenizerWith2spaceIndent(){
+        Line line = new Line("  [Documentation]", 0);
+
+        String[] tokens = line.tokenize();
+
+        assertEquals(tokens.length, 2);
+        assertTrue(tokens[0].isEmpty());
+        assertEquals(tokens[1], "[Documentation]");
+    }
+
+    @Test
+    public void checkTokenizeWith4spaceIndent(){
+        Line line = new Line("    [Documentation]", 0);
+
+        String[] tokens = line.tokenize();
+
+        assertEquals(tokens.length, 2);
+        assertTrue(tokens[0].isEmpty());
+        assertEquals(tokens[1], "[Documentation]");
+    }
+
+    @Test
+    public void checkTokenizeWithTabIndent(){
+        Line line = new Line("\t[Documentation]", 0);
+
+        String[] tokens = line.tokenize();
+
+        assertEquals(tokens.length, 2);
+        assertTrue(tokens[0].isEmpty());
+        assertEquals(tokens[1], "[Documentation]");
+    }
+}
