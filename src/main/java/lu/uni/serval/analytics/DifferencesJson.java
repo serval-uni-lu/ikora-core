82194eb9e8d2fe5a7808473ac793b93a3f4e62ef
kabinja <renaudrwemalika@gmail.com>
2018-09-27 14:02:59 +0200

    export Differences using loc instead of counting just one for each difference

--- src/main/java/lu/uni/serval/analytics/DifferencesJson.java
+++ src/main/java/lu/uni/serval/analytics/DifferencesJson.java
@@ -1,6 +1,7 @@
 package lu.uni.serval.analytics;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import lu.uni.serval.robotframework.model.Element;
 import lu.uni.serval.robotframework.model.Keyword;
 import lu.uni.serval.robotframework.model.KeywordDefinition;
 import lu.uni.serval.utils.Differentiable;
@@ -56,24 +57,37 @@ public class DifferencesJson {
 
     public void add(Difference difference) throws InvalidArgumentException {
         for(Action action: difference.getActions()){
+
+            Differentiable differentiable = action.getValue();
+
+            if(differentiable == null){
+                return;
+            }
+
+            if(!(differentiable instanceof Element)){
+                throw new InvalidArgumentException("Expected a Element got " + differentiable.getClass() + " instead!");
+            }
+
+            Element element = (Element)differentiable;
+
             switch (action.getType()){
                 case ADD_USER_KEYWORD:
-                    actions.put(Type.ADD_USER_KEYWORD, actions.get(Type.ADD_USER_KEYWORD) + 1);
+                    actions.put(Type.ADD_USER_KEYWORD, actions.get(Type.ADD_USER_KEYWORD) + element.getLoc());
                     break;
                 case REMOVE_USER_KEYWORD:
-                    actions.put(Type.REMOVE_USER_KEYWORD, actions.get(Type.REMOVE_USER_KEYWORD) + 1);
+                    actions.put(Type.REMOVE_USER_KEYWORD, actions.get(Type.REMOVE_USER_KEYWORD) + element.getLoc());
                     break;
                 case CHANGE_NAME:
                     actions.put(Type.CHANGE_NAME, actions.get(Type.CHANGE_NAME) + 1);
                     break;
                 case ADD_TEST_CASE:
-                    actions.put(Type.ADD_TEST_CASE, actions.get(Type.ADD_TEST_CASE) + 1);
+                    actions.put(Type.ADD_TEST_CASE, actions.get(Type.ADD_TEST_CASE) + element.getLoc());
                     break;
                 case REMOVE_TEST_CASE:
-                    actions.put(Type.REMOVE_TEST_CASE, actions.get(Type.REMOVE_TEST_CASE) + 1);
+                    actions.put(Type.REMOVE_TEST_CASE, actions.get(Type.REMOVE_TEST_CASE) + element.getLoc());
                     break;
                 case CHANGE_STEP_TYPE:
-                    actions.put(Type.CHANGE_STEP_TYPE, actions.get(Type.CHANGE_STEP_TYPE) + 1);
+                    actions.put(Type.CHANGE_STEP_TYPE, actions.get(Type.CHANGE_STEP_TYPE) + element.getLoc());
                     break;
                 case CHANGE_STEP_RETURN_VALUES:
                     actions.put(Type.CHANGE_STEP_RETURN_VALUES, actions.get(Type.CHANGE_STEP_RETURN_VALUES) + 1);
@@ -82,19 +96,19 @@ public class DifferencesJson {
                     actions.put(Type.CHANGE_FOR_LOOP_CONDITION, actions.get(Type.CHANGE_FOR_LOOP_CONDITION) + 1);
                     break;
                 case CHANGE_FOR_LOOP_BODY:
-                    actions.put(Type.CHANGE_FOR_LOOP_BODY, actions.get(Type.CHANGE_FOR_LOOP_BODY) + 1);
+                    actions.put(Type.CHANGE_FOR_LOOP_BODY, actions.get(Type.CHANGE_FOR_LOOP_BODY) + element.getLoc());
                     break;
                 case ADD_VARIABLE:
-                    actions.put(Type.ADD_VARIABLE, actions.get(Type.ADD_VARIABLE) + 1);
+                    actions.put(Type.ADD_VARIABLE, actions.get(Type.ADD_VARIABLE) + element.getLoc());
                     break;
                 case REMOVE_VARIABLE:
-                    actions.put(Type.REMOVE_VARIABLE, actions.get(Type.REMOVE_VARIABLE) + 1);
+                    actions.put(Type.REMOVE_VARIABLE, actions.get(Type.REMOVE_VARIABLE) + element.getLoc());
                     break;
                 case CHANGE_VARIABLE_DEFINITION:
-                    actions.put(Type.CHANGE_VARIABLE_DEFINITION, actions.get(Type.CHANGE_VARIABLE_DEFINITION) + 1);
+                    actions.put(Type.CHANGE_VARIABLE_DEFINITION, actions.get(Type.CHANGE_VARIABLE_DEFINITION) + element.getLoc());
                     break;
                 case INVALID:
-                    actions.put(Type.INVALID, actions.get(Type.INVALID) + 1);
+                    actions.put(Type.INVALID, actions.get(Type.INVALID) + element.getLoc());
                     break;
 
                 case ADD_STEP:
@@ -102,7 +116,7 @@ public class DifferencesJson {
                 case CHANGE_STEP_EXPRESSION:
                 case CHANGE_STEP:
                 case CHANGE_STEP_ARGUMENTS:
-                    changeStep(action);
+                    changeStep(element);
                     break;
             }
 
@@ -110,43 +124,42 @@ public class DifferencesJson {
         }
     }
 
-    private void changeStep(Action action) throws InvalidArgumentException {
-        Differentiable differentiable = action.getValue();
-
-        if(!(differentiable instanceof Keyword)){
-            throw new InvalidArgumentException("Expected a Keyword got " + differentiable.getClass() + " instead!");
+    private void changeStep(Element element) throws InvalidArgumentException {
+        if(!(element instanceof Keyword)){
+            throw new InvalidArgumentException("Expected a Keyword got " + element.getClass() + " instead!");
         }
 
-        Keyword keyword = (Keyword)differentiable;
+        Keyword keyword = (Keyword)element;
+
 
         switch (keyword.getType()){
 
             case User:
-                actions.put(Type.CHANGE_USER_STEP, actions.get(Type.CHANGE_USER_STEP) + 1);
+                actions.put(Type.CHANGE_USER_STEP, actions.get(Type.CHANGE_USER_STEP) + keyword.getLoc());
                 break;
             case ControlFlow:
-                actions.put(Type.CHANGE_CONTROLFLOW_STEP, actions.get(Type.CHANGE_CONTROLFLOW_STEP) + 1);
+                actions.put(Type.CHANGE_CONTROLFLOW_STEP, actions.get(Type.CHANGE_CONTROLFLOW_STEP) + keyword.getLoc());
                 break;
             case Assertion:
-                actions.put(Type.CHANGE_ASSERT_STEP, actions.get(Type.CHANGE_ASSERT_STEP) + 1);
+                actions.put(Type.CHANGE_ASSERT_STEP, actions.get(Type.CHANGE_ASSERT_STEP) + keyword.getLoc());
                 break;
             case Action:
-                actions.put(Type.CHANGE_ACTION_STEP, actions.get(Type.CHANGE_ACTION_STEP) + 1);
+                actions.put(Type.CHANGE_ACTION_STEP, actions.get(Type.CHANGE_ACTION_STEP) + keyword.getLoc());
                 break;
             case Log:
-                actions.put(Type.CHANGE_LOG_STEP, actions.get(Type.CHANGE_LOG_STEP) + 1);
+                actions.put(Type.CHANGE_LOG_STEP, actions.get(Type.CHANGE_LOG_STEP) + keyword.getLoc());
                 break;
             case Error:
-                actions.put(Type.CHANGE_ERROR_STEP, actions.get(Type.CHANGE_ERROR_STEP) + 1);
+                actions.put(Type.CHANGE_ERROR_STEP, actions.get(Type.CHANGE_ERROR_STEP) + keyword.getLoc());
                 break;
             case Synchronisation:
-                actions.put(Type.CHANGE_SYNC_STEP, actions.get(Type.CHANGE_SYNC_STEP) + 1);
+                actions.put(Type.CHANGE_SYNC_STEP, actions.get(Type.CHANGE_SYNC_STEP) + keyword.getLoc());
                 break;
             case Get:
-                actions.put(Type.CHANGE_GET_STEP, actions.get(Type.CHANGE_GET_STEP) + 1);
+                actions.put(Type.CHANGE_GET_STEP, actions.get(Type.CHANGE_GET_STEP) + keyword.getLoc());
                 break;
             case Unknown:
-                actions.put(Type.CHANGE_UNKNOWN_STEP, actions.get(Type.CHANGE_UNKNOWN_STEP) + 1);
+                actions.put(Type.CHANGE_UNKNOWN_STEP, actions.get(Type.CHANGE_UNKNOWN_STEP) + keyword.getLoc());
                 break;
         }
     }
