a19cab2540c65d83969e4a05abaa0dbb966fe7b2
kabinja <renaudrwemalika@gmail.com>
2018-10-16 15:58:49 +0200

    fix CreateWebdriver name

--- src/main/java/lu/uni/serval/robotframework/libraries/selenium/browserManagement/CreateWebdriver.java
+++ src/main/java/lu/uni/serval/robotframework/libraries/selenium/browserManagement/CreateWebdriver.java
@@ -3,7 +3,7 @@ package lu.uni.serval.robotframework.libraries.selenium.browserManagement;
 import lu.uni.serval.robotframework.model.LibraryKeyword;
 import lu.uni.serval.robotframework.runner.Runtime;
 
-public class CreateWebDriver extends LibraryKeyword {
+public class CreateWebdriver extends LibraryKeyword {
     @Override
     public void execute(Runtime runtime) {
 
