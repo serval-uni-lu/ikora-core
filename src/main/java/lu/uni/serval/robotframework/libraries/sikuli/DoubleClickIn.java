0ea0672b54ddda7261984b0f5ba58962f45f792d
kabinja <renaudrwemalika@gmail.com>
2018-10-22 13:00:00 +0200

    add sikuli keywords

---
+++ src/main/java/lu/uni/serval/robotframework/libraries/sikuli/DoubleClickIn.java
@@ -0,0 +1,11 @@
+package lu.uni.serval.robotframework.libraries.sikuli;
+
+import lu.uni.serval.robotframework.model.LibraryKeyword;
+import lu.uni.serval.robotframework.runner.Runtime;
+
+public class DoubleClickIn extends LibraryKeyword {
+    @Override
+    public void execute(Runtime runtime) {
+
+    }
+}
---
+++ src/main/java/lu/uni/serval/robotframework/libraries/sikuli/StartSikuliProcess.java
@@ -0,0 +1,11 @@
+package lu.uni.serval.robotframework.libraries.sikuli;
+
+import lu.uni.serval.robotframework.model.LibraryKeyword;
+import lu.uni.serval.robotframework.runner.Runtime;
+
+public class StartSikuliProcess extends LibraryKeyword {
+    @Override
+    public void execute(Runtime runtime) {
+
+    }
+}
