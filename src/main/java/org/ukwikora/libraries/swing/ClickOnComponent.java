4fa5576f016fe4bc23941c32b97a25336fb3b376
kabinja <renaudrwemalika@gmail.com>
2019-01-30 12:05:00 +0100

    start add library swing

---
+++ src/main/java/org/ukwikora/libraries/swing/ClickOnComponent.java
@@ -0,0 +1,11 @@
+package org.ukwikora.libraries.swing;
+
+import org.ukwikora.model.LibraryKeyword;
+import org.ukwikora.model.Runtime;
+
+public class ClickOnComponent extends LibraryKeyword {
+    @Override
+    public void execute(Runtime runtime) {
+
+    }
+}
---
+++ src/main/java/org/ukwikora/libraries/swing/ComponentShouldBeVisible.java
@@ -0,0 +1,11 @@
+package org.ukwikora.libraries.swing;
+
+import org.ukwikora.model.LibraryKeyword;
+import org.ukwikora.model.Runtime;
+
+public class ComponentShouldBeVisible extends LibraryKeyword {
+    @Override
+    public void execute(Runtime runtime) {
+
+    }
+}
