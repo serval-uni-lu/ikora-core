f32a1fd7f5be6dd04059ef3fe889e1c292e62396
kabinja <renaudrwemalika@gmail.com>
2019-02-01 16:05:13 +0100

    add process library

---
+++ src/main/java/org/ukwikora/libraries/process/RunProcess.java
@@ -0,0 +1,11 @@
+package org.ukwikora.libraries.process;
+
+import org.ukwikora.model.LibraryKeyword;
+import org.ukwikora.model.Runtime;
+
+public class RunProcess extends LibraryKeyword {
+    @Override
+    public void execute(Runtime runtime) {
+
+    }
+}
---
+++ src/main/java/org/ukwikora/libraries/process/SwitchProcess.java
@@ -0,0 +1,11 @@
+package org.ukwikora.libraries.process;
+
+import org.ukwikora.model.LibraryKeyword;
+import org.ukwikora.model.Runtime;
+
+public class SwitchProcess extends LibraryKeyword {
+    @Override
+    public void execute(Runtime runtime) {
+
+    }
+}
