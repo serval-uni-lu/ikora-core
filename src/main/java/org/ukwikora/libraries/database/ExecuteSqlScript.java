5ebcc0bca1b0d3a2ea2b052047ba07f3b23db30a
kabinja <renaudrwemalika@gmail.com>
2019-02-28 16:52:29 +0100

    add Database library calls

---
+++ src/main/java/org/ukwikora/libraries/database/ExecuteSqlScript.java
@@ -0,0 +1,11 @@
+package org.ukwikora.libraries.database;
+
+import org.ukwikora.model.LibraryKeyword;
+import org.ukwikora.model.Runtime;
+
+public class ExecuteSqlScript extends LibraryKeyword {
+    @Override
+    public void execute(Runtime runtime) {
+
+    }
+}
---
+++ src/main/java/org/ukwikora/libraries/database/RowCountIsLessThanX.java
@@ -0,0 +1,11 @@
+package org.ukwikora.libraries.database;
+
+import org.ukwikora.model.LibraryKeyword;
+import org.ukwikora.model.Runtime;
+
+public class RowCountIsLessThanX extends LibraryKeyword {
+    @Override
+    public void execute(Runtime runtime) {
+
+    }
+}
