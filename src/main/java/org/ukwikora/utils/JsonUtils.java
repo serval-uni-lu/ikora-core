5c912e95ca122f799fb6ff903cc6b3f665b34f02
kabinja <renaudrwemalika@gmail.com>
2019-05-06 17:32:18 +0200

    extend convertToJsonArray to support sets as well

--- src/main/java/org/ukwikora/utils/JsonUtils.java
+++ src/main/java/org/ukwikora/utils/JsonUtils.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class JsonUtils {
     public static String convertToJsonArray(List<?> list) throws IOException {
@@ -14,5 +16,9 @@ public class JsonUtils {
         mapper.writeValue(out, list);
 
         return new String(out.toByteArray());
+    }
+
+    public static String convertToJsonArray(Set<?> list) throws IOException {
+        return convertToJsonArray(new ArrayList<>(list));
     }
 }
