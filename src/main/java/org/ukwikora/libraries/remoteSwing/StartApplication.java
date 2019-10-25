19262271cebd227828881b075a577651a04dd805
kabinja <renaudrwemalika@gmail.com>
2019-10-25 17:14:00 +0200

    add missing keyword from an external library

---
+++ src/main/java/org/ukwikora/libraries/remoteSwing/StartApplication.java
@@ -0,0 +1,11 @@
+package org.ukwikora.libraries.remoteSwing;
+
+import org.ukwikora.model.LibraryKeyword;
+import org.ukwikora.runner.Runtime;
+
+public class StartApplication extends LibraryKeyword {
+    @Override
+    public void run(Runtime runtime) {
+
+    }
+}
