dac7831f326e9d33826513a291319fc7a9b297e1
kabinja <renaudrwemalika@gmail.com>
2021-12-22 12:57:39 +0100

    bump dependencies and update headers

--- src/test/java/lu/uni/serval/ikora/core/model/SourceFileTest.java
+++ src/test/java/lu/uni/serval/ikora/core/model/SourceFileTest.java
@@ -1,5 +1,25 @@
 package lu.uni.serval.ikora.core.model;
 
+/*-
+ * #%L
+ * Ikora Core
+ * %%
+ * Copyright (C) 2019 - 2021 University of Luxembourg
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import lu.uni.serval.ikora.core.builder.BuildResult;
 import lu.uni.serval.ikora.core.builder.Builder;
 import org.junit.jupiter.api.Test;
@@ -42,7 +62,13 @@ class SourceFileTest {
 
         final TestCase test = project.getTestCases().iterator().next();
 
-        assertTrue(sourceFile.get().isImportLibrary(test.getSteps().get(0).getKeywordCall().get().getKeyword().get().getLibraryName()));
+        final Optional<KeywordCall> keywordCall = test.getSteps().get(0).getKeywordCall();
+        assertTrue(keywordCall.isPresent());
+
+        final Optional<Keyword> keyword = keywordCall.get().getKeyword();
+        assertTrue(keyword.isPresent());
+
+        assertTrue(sourceFile.get().isImportLibrary(keyword.get().getLibraryName()));
     }
 
     @Test
